607e08ec4ed4937bdf9c3dff3ad4bba490dbb576
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-03 13:05:10 -0300

    create storage model

---
+++ src/models/Storage.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const storageSchema = new Schema({
+    url : {
+        type: String
+    },
+    filename : {
+        type: String
+    },
+
+},
+{
+    timestamps: true, // createAt, updateAt
+    versionKey: false
+}
+);
+
+const Storage = mongoose.model('Storage', userSchema,'storage');
+
+module.exports = Storage;
