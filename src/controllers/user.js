119f20a5fee369a2236f00adb411d7537a4fa8df
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-03 18:41:54 -0300

    method get one user controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -9,6 +9,14 @@ const controller = {
         res.status(500).json("internal server error");
       }
     },
+    getOne: async (req,res) => {
+      try {
+        const oneUser = await userService.getOne(req,res);
+        res.status(200).json({ data:oneUser});
+      } catch (error) {
+        res.status(500).json("internal server error");
+      }
+    }
 }
   module.exports = controller;
 
