34acbe8c7ef7a5b93b7b1b87aeb11a4084ed79d1
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-03 18:39:44 -0300

    get one user services

--- src/services/user.js
+++ src/services/user.js
@@ -9,4 +9,12 @@ module.exports = {
         res.status(500).json("internal server error");
       }
     },
+    getOne: async (req,res) => {
+      try {
+        const oneUser = await UsersRepository.getOne();
+        res.status(200).json({ data : oneUser });
+      } catch (error) {
+        res.status(500).json("internal server error")
+      }
+    }
 }
