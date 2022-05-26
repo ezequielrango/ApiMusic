f3622d20d8724fd4dd8b42ce0d0f7f90a93f8170
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-26 20:54:07 -0300

    finish development registerControll

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,14 +1,27 @@
 const { matchedData, body } = require("express-validator")
 const  {encrypt,compare} = require('../utils/handlePassword');
 const {usersModel} = require('../models');
+const {tokenSign} = require('../utils/handlerJwt');
+const { handleHttpError } = require("../utils/handleErrors");
 
 const register = async (req , res) => {
-    req = matchedData(req);
-    const passwordHash = await encrypt(req.password);
-    const body = {...req, password: passwordHash };
-    const data = await usersModel.create(body);
-    data.set("password",undefined,{strict: false}); //seteo el modelo para que no retorne el password encriptado, porque el metodo no permite realizar filtrado de atributos
-    res.send({data});
+    try {
+        req = matchedData(req);
+        const passwordHash = await encrypt(req.password);
+        const body = {...req, password: passwordHash };
+        const dataUser = await usersModel.create(body);
+        dataUser.set("password",undefined,{strict: false}); //seteo el modelo para que no retorne el password encriptado, porque el metodo no permite realizar filtrado de atributos
+        
+        const data = {
+            token : await tokenSign(dataUser),
+            user : dataUser
+        };
+    
+        res.send({data});
+        
+    } catch (error) {
+        handleHttpError(res,'Internal Server Error', 500);
+    }
 };
 
 module.exports = { register };
