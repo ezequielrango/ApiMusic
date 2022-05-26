f0773c210d3ce7017ed996219b85619b52a5c070
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-26 18:32:08 -0300

    create auth routes login, import bcrypt function of the handler

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const {validatorRegister,validatorLogin} = require('../validator/auth');
+const {register} = require('../controllers/auth');
 
 
-
-
-router.post('/register');
+router.post('/register',validatorRegister,register);
 
 module.exports = router;
