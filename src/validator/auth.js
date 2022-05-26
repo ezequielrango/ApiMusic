026b7a8985da8d78a33bbb7dcbef87776df0b615
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-26 18:33:08 -0300

    create validation with express for login && register

--- src/validator/auth.js
+++ src/validator/auth.js
@@ -2,10 +2,10 @@ const {check} = require('express-validator');
 const validateResults = require('../utils/handleValidator');
 
 const validatorRegister = [
-    check("name").exists().notEmpty().isLength({min:4, max:35}),
-    check("age").exists().notEmpty().isNumeric(),
-    check("email").exists().notEmpty().isEmail(),
-    check("password").exists().notEmpty().isLength({min:4, max:15}),
+    check("name").exists().notEmpty().isLength({min:4, max:35}).withMessage('min 4 max 35'),
+    check("age").exists().notEmpty().isNumeric().withMessage('only numbers'),
+    check("email").exists().notEmpty().isEmail().withMessage('empty or not is email'),
+    check("password").exists().notEmpty().isLength({min:4, max:15}).withMessage('min 4 max 15'),
 
     (req,res,next) => {
       return  validateResults(req,res,next)
@@ -14,8 +14,8 @@ const validatorRegister = [
 ];
 const validatorLogin = [
 
-    check("email").exists().notEmpty().isEmail(),
-    check("password").exists().notEmpty().isLength({min:4, max:15}),
+    check("email").exists().notEmpty().isEmail().withMessage('empty or not is email'),
+    check("password").exists().notEmpty().isLength({min:4, max:15}).withMessage('min 4 max 15'),
 
 ]
 
