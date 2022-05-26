c553953ceb870722a76b268945afae3cf44aef78
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-26 19:07:12 -0300

    fixed

--- src/utils/handlerJwt.js
+++ src/utils/handlerJwt.js
@@ -4,7 +4,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const {handleHttpError} = require('../utils/handleErrors');
 
 
-const tokenSign = async () => {
+const tokenSign = async (user) => {
 
     const sign = jwt.sign(
         {
