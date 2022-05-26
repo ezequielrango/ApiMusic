1dd5db618c5ca59b8e36109ec19bc13f959a66ee
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-26 18:48:34 -0300

    add comment

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -7,7 +7,7 @@ const register = async (req , res) => {
     const passwordHash = await encrypt(req.password);
     const body = {...req, password: passwordHash };
     const data = await usersModel.create(body);
-    data.set("password",undefined,{strict: false});
+    data.set("password",undefined,{strict: false}); //seteo el modelo para que no retorne el password encriptado, porque el metodo no permite realizar filtrado de atributos
     res.send({data});
 };
 
