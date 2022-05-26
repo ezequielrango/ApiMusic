ca965156db6b0f466a5d755096b7f6e60f551616
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-26 19:04:17 -0300

    create token sign and verify method

---
+++ src/utils/handlerJwt.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const { usersModel } = require('../models');
+const JWT_SECRET = process.env.JWT_SECRET;
+const {handleHttpError} = require('../utils/handleErrors');
+
+
+const tokenSign = async () => {
+
+    const sign = jwt.sign(
+        {
+            _id: user.id,
+            role: user.role
+        },
+        JWT_SECRET,
+        {
+            expiresIn: '2h'
+        }
+    );
+    return sign;   
+};
+
+const verifyToken = async (tokenJwt) => {
+    try {
+        const token = jwt.verify(tokenJwt,JWT_SECRET)
+        if (!token) {
+            handleHttpError(res, 'Token invalid or expired', 401)
+        }
+        return token;
+    } catch (e) {
+        handleHttpError(res, 'Internal Server Error', 500);
+    }
+
+};
+
+module.exports={tokenSign,verifyToken };
