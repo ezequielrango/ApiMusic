5ed6a1468bd66863a0612cb00ef1206c4f8c0193
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-29 20:43:45 -0300

    appĺy filters attributes

--- src/utils/handleTokenUser.js
+++ src/utils/handleTokenUser.js
@@ -2,7 +2,7 @@ const {usersModel} = require('../models');
 
 const getUserByToken = async (tokenId) => {
 
-    const user = await usersModel.findById(tokenId);
+    const user = await usersModel.findById(tokenId).select('name role email');
     return user;
 };
 
