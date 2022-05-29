6df02078ff530abbd00af59bb0cd3d778f08d1ad
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-29 18:18:30 -0300

    sessionValidation, auth

---
+++ src/middlewares/session.js
@@ -0,0 +1,27 @@
+const { handleHttpError } = require("../utils/handleErrors");
+const {verifyToken} = require('../utils/handlerJwt');
+const {getUserByToken} = require('../utils/handleTokenUser');
+
+
+const auth = async (req, res ,next) => {
+    try {
+        const auth = req.headers.authorization;
+         if (!auth) {
+            handleHttpError(res,'No token provided',401);
+            return;
+         }
+         const token = auth.split(' ').pop();
+         const dataToken = await verifyToken(token);
+
+         if (!dataToken._id) {
+            handleHttpError(res,'Error ID Token',401);
+         }
+         const user = getUserByToken(dataToken._id);
+         req.user = user;
+         next();
+    } catch (error) {
+        handleHttpError(res,'Not session',401)
+    };
+};
+
+module.exports = {auth};
