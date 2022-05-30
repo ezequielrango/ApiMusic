c045fee95a8fa40ca000d53322eabf4d01052934
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-30 00:19:11 -0300

    fixed middlware

--- src/routes/tracks.js
+++ src/routes/tracks.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const {getAll,getOne,create,update,remove} = require('../controllers/tracks');
 const {validatorCreate,validatorGetItem} = require('../validator/tracks');
 const {auth} = require('../middlewares/session');
-
+const {checkRol} = require('../middlewares/role');
 
 router.get('/',auth, getAll);
 
 router.get('/:id',auth,validatorGetItem, getOne);
 
-router.post('/',auth,validatorCreate, create);
+router.post('/',auth,checkRol(['admin']),validatorCreate, create);
 
-router.put('/:id',auth,validatorGetItem,validatorCreate, update);
+router.put('/:id',auth,checkRol,validatorGetItem,validatorCreate, update);
 
-router.delete('/:id',auth,validatorGetItem, remove);
+router.delete('/:id',auth,checkRol,validatorGetItem, remove);
 
 module.exports = router;
