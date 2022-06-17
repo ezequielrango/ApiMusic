4dbe3a1bcfba06d6ea236204c39804d82b15dda5
Ezequiel Rango <79542271+ezequielrango@users.noreply.github.com>
2022-06-17 15:29:05 -0300

    Update handleLogger.js

--- src/utils/handleLogger.js
+++ src/utils/handleLogger.js
@@ -1,5 +1,5 @@
 const { IncomingWebhook } = require("@slack/webhook");
-const url = 'https://hooks.slack.com/services/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'
+const url = 'https:/'
 const webHook = new IncomingWebhook(url);
 const loggerStream = {
     write: (message) => {
